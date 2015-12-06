4429400c633ad0f5af2527d14478667cbd2b818f
Evan Butler <evanb2@mac.com>
2015-12-05 18:47:34 -0800

    Add Album module via composer

--- module/Album/Module.php
+++ module/Album/Module.php
@@ -12,21 +12,11 @@ class Module implements AutoloaderProviderInterface, ConfigProviderInterface
 {
     public function getAutoloaderConfig()
     {
-        return array(
-            'Zend\Loader\ClassMapAutoloader' => array(
-                __DIR__ . '/autoload_classmap.php',
-            ),
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-                ),
-            ),
-        );
+        //configured using composer.json
     }
 
     public function getConfig()
     {
-        // TODO: Implement getConfig() method.
         return include __DIR__ . '/config/module.config.php';
     }
 }
