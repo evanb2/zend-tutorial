8bb4c5e4bc3519b8bca58e2347f7308d603c14d5
Evan Butler <ebutler@fuelmedical.com>
2015-12-15 14:28:47 -0800

    Add index.phtml

---
+++ module/Blog/src/Blog/Controller/ListController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: evanbutler
+ * Date: 12/15/15
+ * Time: 2:17 PM
+ */
+
+namespace Blog\Controller;
+
+
+use Zend\Mvc\Controller\AbstractActionController;
+
+class ListController extends AbstractActionController
+{
+
+}
