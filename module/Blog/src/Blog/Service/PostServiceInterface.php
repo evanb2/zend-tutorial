98b4806ffdaf746b4a514dfc1678861d8fdd3898
Evan Butler <ebutler@fuelmedical.com>
2015-12-15 16:46:49 -0800

    Add PostServiceInterface

---
+++ module/Blog/src/Blog/Service/PostServiceInterface.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: evanbutler
+ * Date: 12/15/15
+ * Time: 4:41 PM
+ */
+
+namespace Blog\Service;
+
+use Blog\Model\PostInterface;
+
+interface PostServiceInterface
+{
+    /**
+     * @return array|PostInterface[]
+     */
+    public function findAllPosts();
+
+    /**
+     * @param int $id
+     * @return PostInterface
+     */
+    public function findPost($id);
+}
