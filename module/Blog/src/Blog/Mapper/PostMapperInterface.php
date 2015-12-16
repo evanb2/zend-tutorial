6eb5982d1de47da995a33c4a82ac4deeaec3845c
Evan Butler <ebutler@fuelmedical.com>
2015-12-16 13:51:49 -0800

    Add docblocks to PostMapper

--- module/Blog/src/Blog/Mapper/PostMapperInterface.php
+++ module/Blog/src/Blog/Mapper/PostMapperInterface.php
@@ -11,7 +11,15 @@ use Blog\Model\PostInterface;
 
 interface PostMapperInterface
 {
+    /**
+     * @param int|string $id
+     * @return PostInterface
+     * @throws \InvalidArgumentException
+     */
     public function find($id);
 
+    /**
+     * @return array|PostInterface[]
+     */
     public function findAll();
 }
