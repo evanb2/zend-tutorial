07286a12d22a4a7e8b7a2d7fac43516abb10e7d4
Evan Butler <ebutler@fuelmedical.com>
2015-12-16 15:10:59 -0800

    Implement PostMapper in PostService.php

--- module/Blog/src/Blog/Service/PostService.php
+++ module/Blog/src/Blog/Service/PostService.php
@@ -8,52 +8,30 @@
 
 namespace Blog\Service;
 
-
-use Blog\Model\Post;
-use Blog\Model\PostInterface;
+use Blog\Mapper\PostMapperInterface;
 
 class PostService implements PostServiceInterface
 {
-    protected $data = [
-        [
-            'id'    => 1,
-            'title' => 'Hello World #1',
-            'text'  => 'This is our first blog post!'
-        ],
-        [
-            'id'    => 2,
-            'title' => 'Hello World #2',
-            'text'  => 'This is our second blog post!'
-        ],
-        [
-            'id'    => 3,
-            'title' => 'Hello World #3',
-            'text'  => 'This is our third blog post!'
-        ],
-        [
-            'id'    => 4,
-            'title' => 'Hello World #4',
-            'text'  => 'This is our fourth blog post!'
-        ],
-        [
-            'id'    => 5,
-            'title' => 'Hello World #5',
-            'text'  => 'This is our fifth blog post!'
-        ]
-    ];
+    /**
+     * @var \Blog\Mapper\PostMapperInterface
+     */
+    protected $postMapper;
+
+    /**
+     * PostService constructor.
+     * @param PostMapperInterface $postMapper
+     */
+    public function __construct(PostMapperInterface $postMapper)
+    {
+        $this->postMapper = $postMapper;
+    }
 
     /**
      * @return array|PostInterface[]
      */
     public function findAllPosts()
     {
-        $allPosts = [];
 
-        foreach ($this->data as $index => $post) {
-            $allPosts[] = $this->findPost($index);
-        }
-
-        return $allPosts;
     }
 
     /**
@@ -62,13 +40,6 @@ class PostService implements PostServiceInterface
      */
     public function findPost($id)
     {
-        $postData = $this->data[$id];
 
-        $model = new Post();
-        $model->setId($postData['id']);
-        $model->setTitle($postData['title']);
-        $model->setText($postData['text']);
-
-        return $model;
     }
 }
